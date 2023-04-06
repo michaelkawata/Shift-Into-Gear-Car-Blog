20e897f36d022879a562f493f11d2e259cc1050e
Michael Kawata <michaelkawata@bens-air.lan>
2023-04-05 19:39:15 -0700

    app missing dependencies

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,7 +8,16 @@ const path = require('path')
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// app.use(express.urlencoded({ extended: false }))
+
+// Express Settings
+// Express Settings
+// app.set('views', __dirname + '/views')
+// app.set('view engine', 'jsx')
+// app.engine('jsx', require('express-react-views').createEngine())
+// app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+// app.use(methodOverride('_method'))
 
 // Serve React App
 app.use(express.static(path.join(__dirname, '..', '..', 'dist')));
@@ -20,6 +29,10 @@ app.use('/posts', postsController)
 
 const usersController = require('./controllers/users_controller')
 app.use('/users', usersController)
+
+app.get('/home', (req, res) => {
+    res.render('Home')
+})
 
 // LISTEN
 app.listen(process.env.PORT, () => {
